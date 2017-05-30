824dc803ed83d48aff22ac8c5a29e19a1dc2c58a
lbsn <lucabisin@gmail.com>
2017-05-30 18:30:50 +0200

    Added Square tests

--- src/test/java/world/SquareTest.java
+++ src/test/java/world/SquareTest.java
@@ -7,13 +7,19 @@ import org.junit.Test;
 import cell.*;
 
 public class SquareTest {
+	private World w;
+	private AbstractCell c;
+	private int cellX = 3;
+	private int cellY = 3;
 	private Square s = null;
 	private int x = 4;
 	private int y = 4;
 	
 	@Before
-	public void init(){
+	public void setUp(){
 		this.s = new Square(this.x, this.y);
+		this.w = WorldBuilder.INSTANCE.makeWorld(10, 10, WorldType.WRAP);
+		this.c = new Cell_1(this.cellX, this.cellY, w);
 	}
 	
 	@Test
@@ -23,12 +29,15 @@ public class SquareTest {
 
 	@Test
 	public void testGetOccupiedBy() {
-		assertNull("Should return null", this.s.getOccupiedBy());
+		assertNull("Empty square: should return null", this.s.getOccupiedBy());
+		this.s.setOccupiedBy(this.c);
+		assertEquals(this.c, this.s.getOccupiedBy());
 	}
 
 	@Test
 	public void testSetOccupiedBy() {
-		fail("Not yet implemented");
+		this.s.setOccupiedBy(this.c);
+		assertEquals(this.c, this.s.getOccupiedBy());
 	}
 
 	@Test
